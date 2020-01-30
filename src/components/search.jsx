3fbe77d134e4f5bb8c4b14203b5607539dad18fd
kecygaurab <kecy.gaurab@gmail.com>
2020-01-30 09:50:16 +0200

    congfig and code base for the app

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,72 +1,33 @@
-import React from 'react';
-import {
-  Button,
-  TextField,
-  Select,
-  Grid,
-  MenuItem,
-  InputAdornment,
-} from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-import {StyledButton} from './styledComponents';
+import React from "react";
+import { Button, TextField, Grid, InputAdornment } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
-const Search = ({
-  handleSubmit,
-  handleSearch,
-  handleSearchParameter,
-  query,
-  searchParameter,
-}) => {
+const Search = ({}) => {
   return (
     <Grid container direction="row" alignItems="center">
       <Grid item md={6} xs={12}>
-
         <TextField
-          value={query}
           fullWidth
-          onChange={handleSearch}
           size="medium"
           id="outlined-search"
           type="search"
-          variant="filled"
-          placeholder="Search for books"
+          variant="outlined"
+          placeholder="Search"
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
-                <SearchIcon fontSize="medium" />
+                <SearchIcon fontSize="default" />
               </InputAdornment>
-            ),
+            )
           }}
         />
       </Grid>
       <Grid item md={2} xs={12}>
-        <Button
-          disabled={query ? false : true}
-          value={query}
-          onClick={handleSubmit}
-          size="large"
-          variant="contained"
-          color="primary"
-        >
+        <Button size="large" variant="outlined" color="primary">
           search
         </Button>
-
       </Grid>
-      <Grid item md={3} xs={12}>
-        <Select
-          onChange={handleSearchParameter}
-          defaultValue="Book-Name"
-          variant="outlined"
-        >
-          <MenuItem value="Author">
-            Author
-          </MenuItem>
-          <MenuItem value="Book-Name">
-            Book-Name
-          </MenuItem>
-        </Select>
-
-      </Grid>
+      <Grid item md={3} xs={12}></Grid>
     </Grid>
   );
 };
