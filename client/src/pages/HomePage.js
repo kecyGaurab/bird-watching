69a297476b6c1405c4390a51f50fd75a92b8010c
Gaurab KC <35660893+kecyGaurab@users.noreply.github.com>
2020-08-22 15:21:51 +0300

    Refactor (#22)
    * fixed the frigging eslint to work for now
    
    * Merge branch 'master' of github.com:kecyGaurab/bird-watching
    refactoring done because of frigging eslint
    
    * confirmation dialog for delete

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -8,7 +8,6 @@ import { useDispatch, connect } from 'react-redux';
 import Bird from '../components/Bird/bird';
 import { initializeBirds } from '../redux/reducers/birdReducer';
 import NavBar from '../components/navBar';
-import ImageChange from '../components/Form/imageChange';
 
 const mapStateToProps = (state) => {
   return {
@@ -47,24 +46,6 @@ const HomePage = (props) => {
       <NavBar query={query} handleQueryChange={handleQueryChange} />
       <Container>
         <Grid justify="space-around" container direction="row" spacing={5}>
-          <Grid item xs={12}>
-            {/* <Snackbar
-            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            open={open}
-            autoHideDuration={4000}
-            onClose={handleClose}
-          >
-            {error ? (
-              <Alert onClose={handleClose} severity="error">
-                {message}
-              </Alert>
-            ) : (
-              <Alert onClose={handleClose} severity="success">
-                {message}
-              </Alert>
-            )}
-          </Snackbar> */}
-          </Grid>
           <Grid item xs={12} align="center">
             <Link style={{ textDecoration: 'none' }} to="/add">
               <Button size="large" color="primary" variant="outlined">
