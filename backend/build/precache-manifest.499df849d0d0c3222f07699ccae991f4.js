55fc1a2d4a0d2afaf8bea240a7973bc6d76868a6
kecyGaurab <kecy.gaurab@gmail.com>
2020-08-30 21:31:46 +0300

    uibuild

---
+++ backend/build/precache-manifest.499df849d0d0c3222f07699ccae991f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4d51e729aded47b031a21c67e5d44a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40259cf1510e54274e4b",
+    "url": "/static/js/2.642b5963.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/static/js/2.642b5963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "148c88de5491691eeccb",
+    "url": "/static/js/main.aa0a95d1.chunk.js"
+  },
+  {
+    "revision": "b0bc59b17d0c9f91e49e",
+    "url": "/static/js/runtime-main.288bb335.js"
+  }
+]);
